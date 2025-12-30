9683b08354dc76bc92d8ee23b4d8e5f12c559bbe
Roberts Kamarūts <RobertsKamaruts0@gmail.com>
2025-12-30 08:15:39 +0200

    Pārvietoju definētās koordinātas jaunā failā

--- automatisks_foto.py
+++ automatisks_foto.py
@@ -10,22 +10,23 @@
 #
 # -------------------------------------------------------------------
 #
-# This programme is made as a part of the scientific paper to automate Latvian relief and slope models image capturing. It allows 
-# to generate the dataset on which the paper's algorithm was trained.
+# This program was developed as part of a scientific research paper to automate the image capturing of Latvia's relief and slope models. 
+# It enables the generation of the dataset upon which the study's algorithm was trained.
 # 
-# The programme's different mode code sections are similar to each other, but in each one there are different and specific nuances 
-# to their logic. That's why the shared parts aren't combined in a single function. In this case, this is just the technically 
-# simplest and the most understandable solution.
+# The code sections for the different modes are similar; however, each requires specific nuances in its logic. Therefore, shared code 
+# segments have not been combined into a single function. In this context, this represents the technically simplest and most 
+# understandable solution.
 #
-# Programme created by: Roberts Kamarūts
+# Program created by: Roberts Kamarūts
 #
 # -------------------------------------------------------------------------------------------------------------------------------------
 
-from pyproj import Transformer      # Pārveidos koordinātas no vienas koordinātu sistēmas uz citu.
-from colorama import Fore           # Formatēs tekstu terminālī.
-import requests                     # Iegūs attēlus no WMS servera.
-import math                         # Atļaus apaļot skaitļus.
-import os                           # Darbosies ar mapēm.
+from regioni import pilskalni, defined_territory    # Iegūst definēto pilskalnu un parasto teritoriju koordinātas no regioni.py faila.
+from pyproj import Transformer                      # Pārveidos koordinātas no vienas koordinātu sistēmas uz citu.
+from colorama import Fore                           # Formatēs tekstu terminālī.
+import requests                                     # Iegūs attēlus no WMS servera.
+import math                                         # Atļaus apaļot skaitļus.
+import os                                           # Darbosies ar mapēm.
 
 # Pamatu iestatījumi.
 coordinate_transformer = Transformer.from_crs("EPSG:4326", "EPSG:3059", always_xy=True)     # Pārveidos koordinātas uz LKS-92 metru sistēmu.
@@ -96,7 +97,6 @@ if mode == "1" or mode == "2":
 
 # Pilskalnu režīms.
 if mode == "1":
-
     # Izvēlēto pilskalnu nosaukumi ar to atrašanās koordinātām.
     # Pilskalni, kas tika izvēlēti, bet izņemti no saraksta algoritma pārbaudei:
     #   - "Gribuļu pilskalns": [56.58872, 27.36869]
@@ -104,51 +104,6 @@ if mode == "1":
     #   - "Silaješku pilskalns": [57.15694, 26.26512]
     #   - "Bānūžu pilskalns": [57.15287, 25.57436]
     #   - "Piltiņkalns": [57.49235, 25.04525]
-    pilskalni = {
-        "Barauhas pilskalns": [56.31944, 27.30583], "Vacslobodas": [56.34263, 27.57849], "Smaganu pilskalns": [56.29535, 27.33050], "Cērtenes pilskalns": [57.41027, 25.90032],
-        "Ladušu pilskalns": [56.36972, 27.37672], "Višu pilskalns": [56.38198, 27.56948], "Bondaru pilskalns": [56.29186, 27.33856], "Zeļenopoles Krātavu kalns": [56.26869, 27.49813],
-        "Viraudas pilskalns": [56.24556, 27.40065], "Pušas pilskalni": [56.23960, 27.23403], "Svātovas pilskalns": [56.22021, 27.15559], "Leimanišķu Baterijas kalns": [56.31438, 27.21843],
-        "Leimanišķu pilskalns": [56.31476, 27.22451], "Gušču pilskalns": [56.37484, 27.15020], "Ķīšu kalns": [56.54085, 27.77239], "Degteru pilskalns": [56.56290, 27.87410],
-        "Solomenku pilskalns": [56.38204, 27.70453], "Porkaļu pilskalns": [56.37415, 27.75173], "Divkšu pilskalns": [56.38129, 27.87022], "Pentjušu pilskalns": [56.32437, 27.88384],
-        "Rudavas kalns": [56.33248, 27.91851], "Naumku pilskalns": [56.33997, 28.12059], "Zagorsku pilskalns": [56.29249, 28.04941], "Garaņu pilskalns": [56.23155, 28.16606],
-        "Andžānu pilskalns": [56.11359, 27.92306], "Svātiunes pilskalns": [56.88670, 27.56151], "Dzirkaļu (Gorku) pilskalns": [56.27175, 27.64220], "Ūdrāju pilskalns": [56.21252, 27.62201],
-        "Puncuļevas pilskalns": [56.90325, 27.60679], "Rubenaucu pilskalns": [56.05223, 27.67339], "Jukumu Grumuškas kalns - pilskalns I": [56.18240, 27.53392], "Jukumu Grumuškas kalns - pilskalns II": [56.18353, 27.52740],
-        "Dauguļu Kapču kalns": [56.18132, 27.49384], "Stalidzānu Svilušais kalns": [56.18866, 27.37164], "Černoručjes pilskalns": [56.18058, 27.33139], "Beitānu pilskalns": [56.17134, 27.32554],
-        "Slabadas Augstais kalns": [56.10576, 27.47470], "Ļutovcu Augstais kalns - pilskalns I": [56.04098, 27.55835], "Robežnieku (Pustiņas) pilskalns": [55.96857, 27.61290], "Okras pilskalns": [56.13472, 27.25787],
-        "Vonogu (Vaišļu, Aulejas) pilskalns": [56.04655, 27.26692], "Ļaksu pilskalns": [55.94811, 27.47096], "Udinavas pilskalns": [55.83268, 27.48290], "Lielindricas pilskalns": [55.85779, 27.30245],
-        "Daudzīšu Kazas kalns": [55.87205, 27.33980], "Borovkas pilskalns": [55.89895, 27.26473], "Krāslavas pilskalns": [55.90859, 27.16630], "Kaplavas pilskalns": [55.84951, 26.98472],
-        "Vecračinas pilskalns": [55.91548, 26.93304], "Stanovišku pilskalns": [56.11750, 27.18209], "Brīveru pilskalns": [56.04452, 27.02745], "Gorodokas pilskalns": [56.11596, 27.09642],
-        "Rivarišku pilskalns": [56.11095, 27.08266], "Madalānu pilskalns": [56.12015, 27.04734], "Valaiņu pilskalns": [56.08275, 27.04052], "Dudaru pilskalns": [56.19636, 27.06227],
-        "Rušenicas pilskalns": [56.21590, 27.03197], "Grohoļsku pilskalns": [56.24688, 27.01743], "Kategrades pilskalns": [56.23805, 26.92961], "Silavišku pilskalns": [56.09703, 26.85610],
-        "Križovkas pilskalns": [56.04291, 26.96518], "Leperu pilskalns": [56.03653, 26.92812], "Zabornajas pilskalns": [56.08552, 26.81056], "Baraukas pilskalns": [56.10677, 26.75487],
-        "Liumonu pilskalns": [56.11472, 26.75444], "Kalvānu pilskalns": [56.18103, 26.71688], "Rumpu pilskalns": [56.32913, 26.67485], "Pleskavas pilskalns": [56.62228, 26.95735],
-        "Mistra kalns": [57.02755, 27.58942], "Kauguru kalns": [57.24896, 27.46602], "Tempļa kalns": [57.43224, 27.05780], "Jaunķempju pilskalns": [57.24893, 26.84475],
-        "Lazdu kalns": [57.19766, 26.69814], "Krapas pilskalns": [57.13232, 26.64159], "Liedes kalni": [57.05369, 26.54879], "Obzerkalns": [57.03985, 26.50912],
-        "Atašienes pilskalns": [56.50874, 26.42213], "Ģedušu pilskalns": [56.44796, 26.27879], "Dzenes kalns": [56.20942, 26.26772], "Greizo kalnu pilskalns": [56.09954, 26.27725],
-        "Veipieša pilskalns": [56.00199, 26.56697], "Sīķeles pilskalns": [55.87772, 26.77920], "Lasiņu pilskalns": [55.85403, 26.59758], "Kaķīšu pilskalns": [55.89601, 26.38620],
-        "Ļūbasta pilskalns": [55.94253, 26.45278], "Sudmaļu pilskalns": [55.90071, 26.34321], "Lapsu kalns": [55.97043, 26.18967], "Driģeņu pilskalns": [56.00812, 26.22034],
-        "Sidrabiņu pilskalns": [56.01610, 26.21296], "Palazdiņu pilskalns": [56.02053, 26.29350], "Zamečkas pilskalns": [56.03046, 26.29157], "Rūrānu Batarejas pilskalns": [56.02479, 26.20783],
-        "Liepkalnu pilskalns": [56.03531, 26.19574], "Kaldabruņas pilskalns": [56.09121, 26.08101], "Radžupes pilskalns": [56.17322, 25.77420], "Aknīstes pilskalns": [56.16774, 25.73872],
-        "Stupeļu kalns": [56.16952, 25.46556], "Kņāvu pilskalns": [56.22555, 25.53137], "Skosu pilskalns": [56.26153, 25.36879], "Kūliņu pilskalns": [56.32104, 25.52081],
-        "Sērpiņu pilskalns": [56.41007, 25.52625], "Kaupres pilskalns": [56.47879, 25.92286], "Asotes pilskalns": [56.49295, 25.91890], "Dzirkaļu pilskalns": [56.52447, 26.03437],
-        "Piksteres Zilais kalns": [56.47737, 25.55115], "Grūbeles pilskalns": [56.51164, 25.52261], "Sāvienas pilskalns": [56.66524, 26.19203], "Lazdonas pilskalns": [56.81722, 26.16727],
-        "Dreimaņu pilskalns": [56.76936, 26.13974], "Sauleskalna pilskalns": [56.80367, 26.07326], "Uriekstes pilskalns": [57.20338, 26.32653], "Cauņu pilskalns": [57.43072, 26.19145],
-        "Kaudžu pilskalns": [57.22119, 26.11936], "Dārznīcas kalns": [56.89294, 26.04358], "Kapusila pilskalns": [57.32755, 25.95721], "Pīkaņu Skansts kalns": [57.28350, 25.75021],
-        "Ķētu pilskalns": [57.21370, 25.71922], "Dzērbenes Augstais kalns": [57.19316, 25.67945], "Vējavas pilskalns": [56.92240, 25.89259], "Aderkašu pilskalns": [56.84606, 25.35266],
-        "Pekas kalns": [57.47879, 25.39047], "Sāruma kalns": [57.28655, 25.37127], "Līču pilskalns": [57.20223, 25.32254], "Riekstu kalns": [57.31373, 25.26886],
-        "Vaidavas pilskalns": [57.45811, 25.26905], "Skaņkalnes pilskalns": [57.85956, 25.03238], "Vīkšēnu pilskalns": [57.84875, 25.00507], "Liepupes pilskalns": [57.46825, 24.42740],
-        "Lojas pilskalns": [57.14603, 24.68296], "Grebu Bļodas kalns": [57.62101, 24.99411], "Grebu pilskalns": [57.62046, 24.99594], "Panūtes kalns": [57.35021, 24.92158],
-        "Batarejas kalns Mazstraupē": [57.35931, 24.95947], "Ērgļu kalni": [57.37767, 24.96160], "Ureles pilskalns": [57.35394, 25.07307], "Kvēpenes pilskalns": [57.27685, 25.18396],
-        "Vikmestes pilskalns": [57.17578, 24.82883], "Krusta kalna pilskalns": [57.16775, 24.84835], "Mālpils pilskalns": [57.00920, 24.94803], "Ķoderu pilskalns": [56.90753, 24.77068],
-        "Mežotnes Vīna kalns": [56.43532, 24.04297], "Mežotnes pilskalns": [56.44149, 24.04530], "Sakaiņu pilskalns": [56.80015, 24.37529], "Daugmales pilskalns": [56.83109, 24.38250],
-        "Tukuma pilskalns": [56.96853, 23.12958], "Tērvetes Svētkalns": [56.48475, 23.38293], "Incēnu pilskalns": [56.49996, 22.74298], "Spārnu pilskalns": [56.55046, 23.04290],
-        "Jaunpils Kartavu kalns": [56.71702, 23.01670], "Vecmoku pilskalns": [56.99517, 23.06669], "Pūres pilskalns": [57.03709, 22.93982], "Kandavas pilskalns": [57.04105, 22.78256],
-        "Buses pilskalns": [56.99151, 22.60373], "Sabiles pilskalns": [57.04684, 22.57666], "Dzegužu pilskalns": [57.14022, 22.76354], "Lībagu Kāravkalns": [57.20653, 22.60724],
-        "Laucienes Milzu kalns": [57.23833, 22.64155], "Talsu pilskalns": [57.24266, 22.59917], "Puiškalna pilskalns": [57.54264, 22.51361], "Ugāles pilskalns": [57.28210, 22.03670],
-        "Diždāmes un Aizvīķu pilskalni": [56.33664, 21.71100], "Kazdangas pilskalns": [56.70891, 21.74040], "Ievādes Kapličas kalns": [56.68909, 21.53423], "Vārtājas pilskalns": [56.48831, 21.38163],
-        "Kundu (Turlavas) pilskalns": [56.82172, 21.87871], "Mazsāliju pilskalns": [56.87949, 21.92648], "Lipaiķu pilskalns": [56.84774, 21.77821], "Alšvangas Dižgabalkalns": [56.98091, 21.56932],
-        "Padures pilskalns": [57.04312, 21.92367], "Paventu pilskalns": [57.10043, 21.83887]
-    }
 
     # Attēlu daudzums.
     image_count_per_folder = len(pilskalni)
@@ -206,29 +161,6 @@ if mode == "1":
 
 # Parasto teritoriju režīms.
 if mode == "2":
-    defined_territory = [
-        ["1.", 57.29760, 25.77101, 57.27361, 25.91211],
-        ["2.", 57.30668, 26.00155, 57.29871, 26.06772],
-        ["3.", 57.23783, 26.07987, 57.21842, 26.11493],
-        ["4.", 57.13857, 26.68330, 57.11765, 26.71188],
-        ["5.", 57.10516, 26.93899, 57.06743, 26.99015],
-        ["6.", 57.58237, 26.87994, 57.53762, 26.98963],
-        ["7.", 57.54696, 27.20573, 57.51527, 27.26480],
-        ["8.", 56.85733, 23.61151, 56.83789, 23.63640],
-        ["9.", 56.42729, 21.15263, 56.42444, 21.22825],
-        ["10.", 57.49530, 24.95124, 57.45312, 24.99673],
-        ["11.", 57.48197, 24.99700, 57.43777, 25.12806],
-        ["12.", 56.53874, 25.39340, 56.52002, 25.43594],
-        ["13.", 56.72646, 25.70457, 56.69489, 25.82285],
-        ["14.", 56.55582, 21.60084, 56.54943, 21.64051],
-        ["15.", 56.35836, 21.71618, 56.34264, 21.76932],
-        ["16.", 56.57562, 23.05713, 56.56705, 23.09588],
-        ["17.", 56.27271, 28.00421, 56.25206, 28.03780],
-        ["18.", 56.20647, 27.20648, 56.18225, 27.24296],
-        ["19.", 57.24358, 24.87682, 57.22251, 24.97534],
-        ["20.", 56.88144, 25.93082, 56.86103, 26.00747],
-    ]
-
     image_id_start_territory = image_id_original    # Ar šo mainīgo tiks saglabāts katras nākamās teritorijas pirmā attēla numurs.
     print("")
 
